d36a48d21df7a1c951b29aeb3a8cf34fc86d99f1
Alexey <alexeykapanov@gmail.com>
2017-01-21 23:00:56 +0300

    Codestyle fixed. Replace long to short array

--- .description.php
+++ .description.php
@@ -5,12 +5,12 @@
 use Bitrix\Main\Localization\Loc;
 
 Loc::loadMessages(__FILE__);
-$arComponentDescription = array('NAME'        => Loc::getMessage('SORT_PANEL_COMPONENT_NAME_VALUE'),
-                                'DESCRIPTION' => Loc::getMessage('SORT_PANEL_COMPONENT_DESCRIPTION_VALUE'),
-                                'ICON'        => '/images/icon.gif',
-                                'SORT'        => 10,
-                                'CACHE_PATH'  => 'Y',
-                                'PATH'        => ['ID'    => 'codeblog.pro',
-                                                  'CHILD' => array('ID'   => 'Content',
-                                                                   'NAME' => Loc::getMessage('SORT_PANEL_COMPONENT_TYPE_CONTENT_VALUE'),),],
-                                'COMPLEX'     => 'N',);
+$arComponentDescription = ['NAME'        => Loc::getMessage('SORT_PANEL_COMPONENT_NAME_VALUE'),
+                           'DESCRIPTION' => Loc::getMessage('SORT_PANEL_COMPONENT_DESCRIPTION_VALUE'),
+                           'ICON'        => '/images/icon.gif',
+                           'SORT'        => 10,
+                           'CACHE_PATH'  => 'Y',
+                           'PATH'        => ['ID'    => 'codeblog.pro',
+                                             'CHILD' => ['ID'   => 'Content',
+                                                         'NAME' => Loc::getMessage('SORT_PANEL_COMPONENT_TYPE_CONTENT_VALUE'),],],
+                           'COMPLEX'     => 'N',];
